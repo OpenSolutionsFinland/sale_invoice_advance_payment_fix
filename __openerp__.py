d961210df942d6bef1f89ce9e8aa919733512298
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-01-23 16:14:51 +0200

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#    Copyright (C) 2013 Open Solutions Finland
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Sale advance payment fix",
+    "version": "1.0",
+    "author": "Open Solutions Finland",
+    "category": "Sale",
+    "depends": ['product', 'sale'],
+    "description": """Fixes advance payment with percentages""",
+    "init_xml": [],
+    "update_xml": [
+
+    ],
+    "demo_xml": [],
+    "active": False,
+    "installable": True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
